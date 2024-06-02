c1fd055c4310dd766e7066d6330b5ded5a231868
NQuangHuyZ31 <huynguyenharu3108@gmail.com>
2024-06-02 22:17:54 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useCallBack,
+  useMemo
+  
+} from 'react';
+
 
 function App() {
+  
+    const [state, setState] = useState(1)
+    const handleIncrease = ()=>{
+      setState(preState => preState +1)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding:20}}>
+      <h1>{state}</h1>
+      <button onClick = {handleIncrease}>Increase</button>
     </div>
   );
 }
