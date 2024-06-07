4ff845742284e100417b4d967849b24976a6a10c
NQuangHuyZ31 <huynguyenharu3108@gmail.com>
2024-06-07 18:39:23 +0700

    config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import TodoApp from "./Todo";
+import { Routes, Route, Link } from 'react-router-dom';
+import { useRef, useImperativeHandle, useEffect } from 'react';
 
-function App(){
-  
-  return <TodoApp/>
+function App() {
+  return (
+    <div style={{ padding: 20 }} className="App">
+      <h1>He lo</h1>
+    </div>
+  );
 }
 
 export default App;
